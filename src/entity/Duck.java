9c141b4173a9baabe4ff98d5c6254d63be221f1c
Your Name <you@example.com>
2015-12-07 11:30:45 +0100

    implement duck move & update

--- src/entity/Duck.java
+++ src/entity/Duck.java
@@ -1,9 +1,13 @@
 package entity;
 
 import data.Status;
+import map.Block;
 import map.Map;
 import virus.H5N1;
 import virus.Virus;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by FILIOL DE RAIMOND-MICHEL Guillaume on 23/11/2015.
@@ -31,11 +35,52 @@ public class Duck implements Entity{
     @Override
     public void move(Map m) {
 
+        if (this.getStatus().equals(Status.DEAD))return;
+        ArrayList<Block> possibilities=new ArrayList<>();
+        int width=this.getWidth();
+        int height=this.getHeight();
+        if (width+1<m.getWidth() && m.getBlock(width+1, height).getEntity() == null){
+            possibilities.add(m.getBlock(width+1, height));
+        }
+        if (height+1<m.getHeight() && m.getBlock(width, height+1).getEntity() == null){
+            possibilities.add(m.getBlock(width, height+1));
+        }
+        if (width-1 >=0 && m.getBlock(width-1, height).getEntity() == null){
+            possibilities.add(m.getBlock(width-1, height));
+        }
+        if (height-1 >=0 && m.getBlock(width, height-1).getEntity() == null){
+            possibilities.add(m.getBlock(width, height-1));
+        }
+        if (possibilities.size()==0)return;
+        Random r=new Random();
+        int rand=(int)r.nextFloat()*possibilities.size();
+        m.remove(width, height);
+        this.setHeight(possibilities.get(rand).getHeight());
+        this.setWidth(possibilities.get(rand).getWidth());
+        m.add(this, this.getWidth(), this.getHeight());
+
     }
 
     @Override
     public void update(Map m) {
+        Block tmp;
+        if (this.getVirus() != null){
+            this.getVirus().time(this, m);
 
+        }
+        if (this.getStatus().equals(Status.CONTAGIOUS)) {
+            for (int i = -1; i < 2; i++) {
+                for (int j = -1; j < 2; j++) {
+                    if (this.getWidth() + i > 0 && this.getHeight() + j > 0 && this.getWidth() + i < m.getWidth() && this.getHeight() + j < m.getHeight()) {
+                        tmp = m.getBlock(this.getWidth() + i, this.getHeight() + j);
+                        Entity e = tmp.getEntity();
+                        if (e != null && this.getVirus() != null) {
+                            this.getVirus().infect(this, e, m);
+                        }
+                    }
+                }
+            }
+        }
     }
 
     /**
